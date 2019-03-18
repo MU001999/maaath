f50bea2d2d25082870a855ca5c8f7400175d0a5d
GEN <956441445@qq.com>
2019-03-18 20:56:27 +0800

    Changes to be committed: 	modified:   Invertedindex.hpp

--- src/Invertedindex.hpp
+++ src/Invertedindex.hpp
@@ -1,13 +1,15 @@
-#include<iostream>
 #include<map>
 #include<vector>
-using namespace std;
-class Invertedindex{
+#include<set>
+class InvertedIndex
+{
 	private:
-		map<int,vector<int>> index; 
+		map<int,set<int>> index;//索引中每个词id对应一个文件id列表
 	public:
-		Invertedindex();
-		vector<int> Getlist(int id);
-		void Setfile(int id,vector<int>filelist);
-		void change(int id,int fileid); 
+		InvertedIndex();
+		set<int> get_file_list(int id);
+		void add_file(int id,int fileid);
+		map<int,int> get_word_interval(utf8 file,int fileid,map<string,double>word);//这里的file utf8类
+		vector<int> get_best_interval(int nowpos,vector<vector<int>>& interval_list,wordfreq word);//word 词频字频类
+
 };
