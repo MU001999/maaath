a32dac7495a55bded18eaedf3dfb32e4c8339f6e
mu001999 <mu001999@outlook.com>
2019-03-19 16:52:13 +0800

    add some comments for utf8string

--- src/utf8string.hpp
+++ src/utf8string.hpp
@@ -17,20 +17,26 @@ private:
     raw_type raw_string;
 
 public:
+    // This is a special value equal to the maximum value representable by the type size_type. The exact meaning depends on context, but it is generally used either as end of string indicator by the functions that expect a string index or as the error indicator by the functions that return a string index.
     static const size_type npos = -1;
 
 
+    // Constructor, receives std::vector<value_type>
     Utf8String(std::vector<value_type> &&data) noexcept;
 
-    // Constructor, receives std::string
+    // Constructor, receives raw_string
     Utf8String(const raw_type &raw_string);
 
+    // Constructor, receives raw_string
     Utf8String(const char *raw_string);
 
+    // Copy constructor
     Utf8String(const Utf8String &rhs);
 
+    // Move constructor
     Utf8String(Utf8String &&rhs) noexcept;
 
+    // Destructor, destroys the string, deallocating internal storage if used
     ~Utf8String();
 
 
@@ -42,16 +48,20 @@ public:
 
     Utf8String& operator=(Utf8String &&rhs) noexcept;
 
+    // Accesses the specified character
     const_reference operator[](std::size_t index) const;
 
 
     // Returns a substring [pos, pos+count). if the requested substring extends past the end of the string, or if count == npos, the returned substring is [pos, size()).
     Utf8String substr(size_type pos = 0, size_type count = npos) const;
 
+    // Returns the number of characters
     size_type size() const noexcept;
 
+    // Returns the raw string
     raw_type raw() const noexcept;
 
+    // Returns a non-modifiable standard C character array version of the string
     const char* c_str() const noexcept;
 
 
