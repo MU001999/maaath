662e1b03af8b0f98b4a7c63ee663d6cb4b6e6512
mu001999 <mu001999@outlook.com>
2019-03-22 12:02:08 +0800

    rm some chinese comments

--- src/invertedindex.hpp
+++ src/invertedindex.hpp
@@ -3,10 +3,10 @@
 class InvertedIndex
 {
 private:
-    std::map<Utf8String, std::set<int>> index;//索引中每个词id对应一个文件id列表
+    std::map<Utf8String, std::set<int>> index; // Each word id in the index corresponds to a list of file ids
 
 public:
     InvertedIndex();
     std::set<int> get_file_list(Utf8String utf8_string);
-    void add_file(Utf8String utf8_string, int file_id, InfoQuantity dict);//utf8_string 词
+    void add_file(Utf8String utf8_string, int file_id, InfoQuantity dict); // utf8_string word
 };
