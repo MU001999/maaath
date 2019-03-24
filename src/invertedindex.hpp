e31480e63cdf68e46672616a1a261d75f7d95fad
mu001999 <mu001999@outlook.com>
2019-03-24 14:10:04 +0800

    update commonly

--- src/invertedindex.hpp
+++ src/invertedindex.hpp
@@ -11,6 +11,6 @@ private:
     static std::map<key_type, value_type> files; // Each word id in the index corresponds to a list of file ids
 
 public:
-    static value_type get_files_list(const key_type &utf8_string);
-    static void add_file(const key_type &utf8_string, int file_id); // utf8_string word
+    static value_type get_files_list(const key_type &sentence);
+    static void add_file(const key_type &sentence, int file_id); // utf8_string word
 };
