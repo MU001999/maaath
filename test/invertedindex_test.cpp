9c4a27833c3bdd6a427b5663b1826ee500c9bd58
mu001999 <mu001999@outlook.com>
2019-03-24 16:22:38 +0800

    update test

---
+++ test/invertedindex_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// #define DEBUG
+
+#include "../src/utf8string.cpp"
+#include "../src/infoquantity.cpp"
+#include "../src/segmentation.cpp"
+#include "../src/invertedindex.cpp"
+
+
+int main()
+{
+    return 0;
+}
--- test/segmentation_test.cpp
+++ test/segmentation_test.cpp
@@ -5,7 +5,6 @@
 #include "../src/utf8string.cpp"
 #include "../src/infoquantity.cpp"
 #include "../src/segmentation.cpp"
-#include "../src/invertedindex.cpp"
 
 
 #define TEST(NAME) static int NAME = []{
@@ -14,10 +13,10 @@
 
 TEST(GETSEGMENT)
     for (auto &s : Segmentation::segment("记者读报道的来源是呱呱食堂")) 
-        std::cout << s.word.raw() << std::endl;
+        std::cout << s.raw() << std::endl;
 
     for (auto &s : Segmentation::segment("要长大，要可爱，但不要变成各种奇奇怪怪的不可爱的人"))
-        std::cout << s.word.raw() << std::endl;
+        std::cout << s.raw() << std::endl;
 END()
 
 
