4b1dcb44ee1bdf46b9ddef8f39f2b94bd0b0d228
mu001999 <mu001999@outlook.com>
2019-04-09 11:10:26 +0800

    add comments

--- src/infoquantity.cpp
+++ src/infoquantity.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 
 
+// Temporary class
 static class _InfoQuantity
 {
 private:
@@ -12,35 +13,33 @@ private:
 
 public:
 
-	_InfoQuantity(Utf8String filename1 = "../resources/charfreq.txt", Utf8String filename2 = "../resources/wordfreq.txt")
+	// Constructor, receives the freq files
+	_InfoQuantity(const std::string &filename1 = "../resources/charfreq.txt", const std::string &filename2 = "../resources/wordfreq.txt")
 	{
-		std::ifstream fin;
 		std::string word;
 		double freq, sum;
-		fin.open(filename1.raw(), std::ios::in);
+
+		std::ifstream fin(filename1);
 		if (!fin) std::cout << "Error opening1" << std::endl;
 		fin >> word >> sum;
-		while (fin >> word >> freq)
-		{
-			info_quantity_[word] = log(sum / freq);
-		}
-		fin.close();
-		fin.open(filename2.raw(), std::ios::in);
+		while (fin >> word >> freq) info_quantity_[word] = log(sum / freq);
+
+		fin.open(filename2);
 		if (!fin) std::cout << "Error opening2" << std::endl;
 		fin >> word >> sum;
-		while (fin >> word >> freq)
-		{
-			info_quantity_[word] = log(sum / freq);
-		}
-		fin.close();
+		while (fin >> word >> freq) info_quantity_[word] = log(sum / freq);
 	}
+
+	// Destructor
 	~_InfoQuantity() {}
 
+	// Returns the infoquantity of given word
 	double get_infoquantity(const Utf8String & word)
 	{
 		return info_quantity_[word];
 	}
 
+	// Returns the number of elements matching specific word
 	bool count(const Utf8String & word) const
 	{
 		return info_quantity_.count(word);
--- src/infoquantity.hpp
+++ src/infoquantity.hpp
@@ -3,7 +3,9 @@
 
 struct InfoQuantity
 {
+	// Returns the infoquantity of given word
 	static double get_infoquantity(const Utf8String& word);
 
+	// Returns the number of elements matching specific word
 	static bool count(const Utf8String& word);
 };
