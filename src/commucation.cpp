ca60a0c80b900e99672a393575f0783de0254ae1
mu001999 <mu001999@outlook.com>
2019-04-11 13:19:11 +0800

    init commucation

---
+++ src/commucation.cpp
@@ -0,0 +1,27 @@
+#include "commucation.hpp"
+
+
+CommProtocol::CommProtocol(const std::string &raw) : type_(static_cast<TYPE>(raw[0] - '0')), content_(raw.substr(1))
+{
+    // ...
+} 
+
+CommProtocol::CommProtocol(TYPE type, const std::string &content) : type_(type), content_(content)
+{
+    // ...
+}
+
+std::string CommProtocol::to_string() const
+{
+    return std::to_string(type_) + content_;
+}
+
+std::string CommProtocol::to_string(const CommProtocol &commp)
+{
+    return commp.to_string();
+}
+
+std::string CommProtocol::to_string(TYPE type, const std::string &content)
+{
+    return CommProtocol(type, content).to_string();
+}
--- src/commucation.hpp
+++ src/commucation.hpp
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <string>
+
+
+class CommProtocol
+{
+public:
+    enum TYPE
+    {
+        Article,
+        Definition,
+        Property,
+        Example,
+        Exercise
+    };
+
+    CommProtocol(const std::string &raw);
+    CommProtocol(TYPE type, const std::string &content);
+    ~CommProtocol() = default;
+
+    std::string to_string() const;
+
+    static std::string to_string(const CommProtocol &commp);
+    static std::string to_string(TYPE type, const std::string &content);
+
+private:
+    TYPE type_;
+    std::string content_;
+};
