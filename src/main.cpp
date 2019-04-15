b588f89af9b496159ea98834b3d2a1f9f462a12a
mu001999 <mu001999@outlook.com>
2019-04-15 11:11:15 +0800

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,37 @@
+#include <array>
+#include <string>
+
+#include "invertedindex.hpp"
 #include "search_server.hpp"
 
 
+std::array<InvertedIndex, 5> iis = {
+    InvertedIndex("../temp/article.tmp"),
+    InvertedIndex("../temp/definition.tmp"),
+    InvertedIndex("../temp/property.tmp"),
+    InvertedIndex("../temp/example.tmp"),
+    InvertedIndex("../temp/exercise.tmp"),
+};
+
+static std::array<std::string, 5> _folderpaths = {
+    "../resources/article",
+    "../resources/definition",
+    "../resources/property",
+    "../resources/example",
+    "../resources/exercise"
+};
+
+static Server _server;
+
 int main(int argc, char *argv[])
 {
-    Server server;
-    server.listen();
-    server.run();
+    for (std::size_t i = 0; i < 5; ++i)
+    {
+        if (!iis[i].ready()) iis[i].add_files(_folderpaths[i]);
+    }
+
+    _server.listen();
+    _server.run();
 
     return 0;
 }
