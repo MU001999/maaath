270735bedb88a562c1ec0d99a6f66501b9577e3b
mu001999 <mu001999@outlook.com>
2019-04-16 11:37:24 +0800

    update protocol

--- src/commucation.hpp
+++ src/commucation.hpp
@@ -14,11 +14,7 @@ abstract#...#
 ...
 
 Response4knowgraph:
-keywordpair#...# (first pair of concepts, delim is '-', for example, CONCEPT1-CONCEPT2)
-keywordpair#...# (second ...)
-...
-keywordnum#...# (count of first concept, delim is '-', for example, CONCEPT-10)
-keywordnum#...# (count of second ...)
+keyword#...# (first keyword after segmentation)
 ...
 */
 
