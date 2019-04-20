c0123cbbddba1a7b19444aa392d2a78bcd44e5f2
mu001999 <mu001999@outlook.com>
2019-04-20 10:18:56 +0800

    change to daemon if not when debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <array>
 #include <string>
 
+#include <unistd.h>
+
 #include "invertedindex.hpp"
 #include "search_server.hpp"
+
+// #define _DEBUG
 
 
 std::array<InvertedIndex, 5> iis = {
@@ -23,6 +27,10 @@ static std::array<std::string, 5> _folderpaths = {
 
 int main(int argc, char *argv[])
 {
+#ifndef _DEBUG
+    daemon(0, 0);
+#endif
+
     for (std::size_t i = 0; i < 5; ++i)
     {
         if (!iis[i].ready()) iis[i].add_files(_folderpaths[i]);
