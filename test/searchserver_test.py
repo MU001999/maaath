fcea11f96f0364f45acb167c53bb315b2d0a8599
mu001999 <mu001999@outlook.com>
2019-04-21 17:23:20 +0800

    add tester for searchserver

--- test/searchserver_test.py
+++ test/searchserver_test.py
@@ -1,14 +1,24 @@
 import socket
 
-def test_ConceptMap(s):
+
+def test(s):
 	sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 	sock.connect("/tmp/datastructureexpt.socket")
 	sock.sendall(s.encode('utf-8'))
-	recv = sock.recv(4096)
+	recv = sock.recv(8192)
 	sock.close()
-	return recv.decode('utf-8')
+	return "Request: " + s + "\nResponse: " + recv.decode('utf-8')
 
 
 if __name__ == "__main__":
-	print(test_ConceptMap("5记者读报道的来源是呱呱食堂"))
-	print(test_ConceptMap("5要长大，要可爱，但不要变成各种奇奇怪怪的不可爱的人"))
+	# print(test("5数轴有理数绝对值"))
+	# print(test("0数轴有理数绝对值"))
+
+	# print(test("1数轴有理数绝对值"))
+	# print(test("2数轴有理数绝对值"))
+	# print(test("3数轴有理数绝对值"))
+	print(test("4数轴有理数绝对值"))
+
+	print(test("5数轴有理数绝对值"))
+	print(test("5记者读报道的来源是呱呱食堂"))
+	print(test("5要长大，要可爱，但不要变成各种奇奇怪怪的不可爱的人"))
