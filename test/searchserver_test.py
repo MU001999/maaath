768fb9c5521b0e04a45ca15152a7c17b0528e1ef
mu001999 <mu001999@outlook.com>
2019-04-22 09:54:25 +0800

    update tester for search server

--- test/searchserver_test.py
+++ test/searchserver_test.py
@@ -1,4 +1,5 @@
 import socket
+from collections import defaultdict
 
 
 def test(s):
@@ -7,18 +8,22 @@ def test(s):
 	sock.sendall(s.encode('utf-8'))
 	recv = sock.recv(8192)
 	sock.close()
-	return "Request: " + s + "\nResponse: " + recv.decode('utf-8')
+	recv = recv.decode('utf-8').split('#')
+	res = defaultdict(list)
+	for i in range(0, len(recv) - 1, 2):
+		res[recv[i]].append(recv[i+1])
+	return res
 
 
 if __name__ == "__main__":
 	# print(test("5数轴有理数绝对值"))
-	# print(test("0数轴有理数绝对值"))
+	print(test("0数轴有理数绝对值"))
 
 	# print(test("1数轴有理数绝对值"))
 	# print(test("2数轴有理数绝对值"))
 	# print(test("3数轴有理数绝对值"))
-	print(test("4数轴有理数绝对值"))
+	# print(test("4数轴有理数绝对值"))
 
-	print(test("5数轴有理数绝对值"))
-	print(test("5记者读报道的来源是呱呱食堂"))
-	print(test("5要长大，要可爱，但不要变成各种奇奇怪怪的不可爱的人"))
+	# print(test("5数轴有理数绝对值"))
+	# print(test("5记者读报道的来源是呱呱食堂"))
+	# print(test("5要长大，要可爱，但不要变成各种奇奇怪怪的不可爱的人"))
