a3dbd533c954940909146834b6f58a2a84c0b36a
mu001999 <mu001999@outlook.com>
2019-05-27 11:02:05 +0800

    update priority

--- src/priority.cpp
+++ src/priority.cpp
@@ -11,40 +11,16 @@ Priority::Priority()
     while (infile >> optr)
     {
         infile >> prty;
-        ITEM item(optr, prty);
-        this->table.insert(item);
+        table[optr] = prty;
     }
-}
-
-char Priority::GetPriority(string optr1, string optr2)
-{
-    int result1, result2;
-    TABLE::iterator it;
-
-    it = this->table.find(optr1);
-    if (it != this->table.end()) result1 = it->second;
-    else return '\0';
-
-    it = this->table.find(optr2);
-    if (it != this->table.end()) result2 = it->second;
-    else return '\0';
-
-    if (result1 - result2 > 0) return '>';
-    else if (result1 - result2 < 0) return '<';
-    else return '=';
 }
 
 bool Priority::is_optr(string str)
 {
-    TABLE::iterator it;
-
-    it = this->table.find(str);
-    return it != this->table.end();
+    return table.count(str);
 }
 
 int Priority::get_pri(string str)
 {
-    TABLE::iterator it;
-    it = this->table.find(str);
-    return it->second;
+    return table[str];
 }
--- src/priority.hpp
+++ src/priority.hpp
@@ -6,17 +6,13 @@
 #include <cstdio>
 #include <map>
 
-typedef std::pair<std::string, int> ITEM;
-typedef std::map<std::string, int> TABLE;
-
 class Priority
 {
 private:
-    TABLE table;
+    std::map<std::string, int> table;
 
 public:
     Priority();
-    char GetPriority(std::string, std::string);
     bool is_optr(std::string str);
     int get_pri(std::string);
 };
